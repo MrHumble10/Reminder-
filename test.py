81cbc0a782821b098cb437628a79574cf0bc88cb
Reza Foroutan <MrHumble10>
2024-03-05 15:44:08 -0300

    modify data

--- test.py
+++ test.py
@@ -156,24 +156,8 @@ def register():
     return render_template("register.html")
 
 
-@app.route("/", methods=["GET", "POST"])
-def login():
-
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['pswd']
-        result = db.session.execute(db.Select(User).where(User.email == email))
-        user = result.scalar()
-        if not user:
-            flash("Email does not exist.")
-            return redirect(url_for('login'))
-        elif not check_password_hash(user.password, password):
-            flash("The password is wrong.")
-            return redirect(url_for('login'))
-        else:
-            login_user(user)
-            return redirect(url_for('home'))
-
+@app.route("/email123456789", methods=["GET", "POST"])
+def admin_email():
     result = db.session.execute(db.select(Todos))
     all_todos = result.scalars().all()
     tomorrow_todos = []
@@ -192,10 +176,52 @@ def login():
 
             # todo_title is in html because email message is in html format\.
             todo_title = f"""
-                                                <li class="mb-2">{item.info}.</li>
-                                            """
+                                                    <li class="mb-2">{item.info}.</li>
+                                                """
             print(f"{user.id}-{todo_title}")
             send_email(user.username, user.email, user.tel, msg=todo_title)
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['pswd']
+        result = db.session.execute(db.Select(User).where(User.email == email))
+        user = result.scalar()
+        if not user:
+            flash("Email does not exist.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("The password is wrong.")
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+
+    # result = db.session.execute(db.select(Todos))
+    # all_todos = result.scalars().all()
+    # tomorrow_todos = []
+    # for date in all_todos:
+    #     if date.due_date == TOMORROW.strftime('%Y-%m-%d'):
+    #         tomorrow_todos = db.session.execute(
+    #             db.select(Todos).where(Todos.due_date == TOMORROW.strftime('%Y-%m-%d'))).scalars().all()
+    # print(tomorrow_todos)
+    #
+    # #  preparing title to send to log in user
+    # if tomorrow_todos:
+    #     todo_title = ''
+    #     users = []
+    #     for item in tomorrow_todos:
+    #         user = db.get_or_404(User, item.user_id)
+    #
+    #         # todo_title is in html because email message is in html format\.
+    #         todo_title = f"""
+    #                                             <li class="mb-2">{item.info}.</li>
+    #                                         """
+    #         print(f"{user.id}-{todo_title}")
+    #         send_email(user.username, user.email, user.tel, msg=todo_title)
 
     return render_template('login.html', logged_in=current_user.is_authenticated)
 
